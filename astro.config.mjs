0420961846178a95cf61f05792d541044b8a2532
weitingshih <weitingshih@softnext-inc.com>
2025-05-15 22:11:12 +0800

    feat: 環境架構設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
+import node from '@astrojs/node';
+
+import db from '@astrojs/db';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), db()],
+
+  adapter: node({
+    mode: 'standalone'
+  })
+});
