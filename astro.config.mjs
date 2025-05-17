60297c7877266ff8c8745d309299ae264bcf1f26
weitingshih <weitingshih@softnext-inc.com>
2025-05-17 12:08:57 +0800

    feat: Taiwindcss daydyui 相關套件設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import node from '@astrojs/node';
+import node from "@astrojs/node";
 
-import db from '@astrojs/db';
+import db from "@astrojs/db";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), db()],
 
   adapter: node({
-    mode: 'standalone'
-  })
+    mode: "standalone",
+  }),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
---
+++ src/components/Signin.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Signin = () => {
+  React.useEffect(() => {
+    console.log("after mounteded....")
+  })
+
+  return (
+    <>
+      <div className="btn btn-error">Signin</div>
+    </>
+  )
+}
+
+export default Signin
+
